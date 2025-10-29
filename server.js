cc85a9c498fac2544ed9dc477c6a26eb942043d5
SNIZER-fr <etoile-noire62@hotmail.fr>
2025-10-29 09:44:47 +0100

    Fix route wildcard pour Express 5

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname)));
 // === Routes ===
 
 // Page d'accueil
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
